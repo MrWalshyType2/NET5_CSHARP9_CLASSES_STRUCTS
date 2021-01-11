f92b4cdf0c28c0563d75d9d8b9d4049cda0bb369
MrWalshyType2 <mwalsh@academytrainee.com>
2021-01-11 17:05:46 +0000

    initial commit classes and structs

---
+++ Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndStructs
+{
+    abstract class Animal
+    {
+        public Animal()
+        {
+            Breed = "Unknown";
+        }
+
+        public Animal(string breed)
+        {
+            breed = breed;
+        }
+
+        // Auto-implemented readonly property
+        //  - Creates a private field in the background
+        public string Breed { get; }
+
+        // Override base class 'System.Object.ToString'
+        public override string ToString()
+        {
+            return Breed;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassesAndStructs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
